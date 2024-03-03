98d1b69fdecbf6801d216d43bf88a21f528781bf
Rustam <rustamb.dev@yandex.ru>
2024-03-03 22:29:02 +0500

    Init commit

---
+++ src/main/java/ru/rustam/LightDigital/LightDigitalApplication.java
@@ -0,0 +1,13 @@
+package ru.rustam.LightDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightDigitalApplication.class, args);
+	}
+
+}
