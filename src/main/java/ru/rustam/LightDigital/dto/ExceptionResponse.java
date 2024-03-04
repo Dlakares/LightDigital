e0b35dfe9e3d9f4c958ac9aa056f20d559d03450
Rustam <rustamb.dev@yandex.ru>
2024-03-04 21:14:27 +0500

    Add global exception handler

---
+++ src/main/java/ru/rustam/LightDigital/dto/ExceptionResponse.java
@@ -0,0 +1,10 @@
+package ru.rustam.LightDigital.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ExceptionResponse {
+    private String msg;
+}
